f039ed59e2d5f2171091b8707d6e0b6296f3b204
MasterX01 <aakashsaxena14@gmail.com>
2021-03-24 19:09:28 +0530

    [Akash] FIRST COMMIT

---
+++ src/main/java/com/bridgelabz/genericsmaximumtest/GenericsMaximum.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.genericsmaximumtest;
+
+public class GenericsMaximum {
+
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Generics Maximum Program");
+    }
+}
