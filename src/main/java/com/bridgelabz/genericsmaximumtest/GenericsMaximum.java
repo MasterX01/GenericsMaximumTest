31d1689a6f7bf3d323281564abb9d48efae15600
MasterX01 <aakashsaxena14@gmail.com>
2021-03-24 19:33:18 +0530

    [Akash] ADD. working UC-2 code

--- src/main/java/com/bridgelabz/genericsmaximumtest/GenericsMaximum.java
+++ src/main/java/com/bridgelabz/genericsmaximumtest/GenericsMaximum.java
@@ -11,6 +11,15 @@ public class GenericsMaximum {
         return max;
     }
 
+    public Float testMax(Float x, Float y, Float z){
+        Float max = x;
+        if(y.compareTo(max) > 0)
+            max = y;
+        if(z.compareTo(max) > 0)
+            max = z;
+        return max;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Generics Maximum Program");
         GenericsMaximum genericsMaximum = new GenericsMaximum();
--- src/test/java/com/bridgelabz/genericsmaximumtest/GenericsMaximumTest.java
+++ src/test/java/com/bridgelabz/genericsmaximumtest/GenericsMaximumTest.java
@@ -8,21 +8,38 @@ public class GenericsMaximumTest {
     GenericsMaximum genericsMaximum = new GenericsMaximum();
 
     @Test
-    public void givenIntegers_whenFirstIntegerIsMax_ReturnSame() {
+    public void givenIntegers_whenFirstNumberIsMax_ReturnSame() {
         Integer result = genericsMaximum.testMax(5, 3, 2);
         Assertions.assertEquals(5, result);
     }
 
     @Test
-    public void givenIntegers_whenSecondIntegerIsMax_ReturnSame() {
+    public void givenIntegers_whenSecondNumberIsMax_ReturnSame() {
         Integer result = genericsMaximum.testMax(3, 5, 2);
         Assertions.assertEquals(5, result);
     }
 
     @Test
-    public void givenIntegers_whenThirdIntegerIsMax_ReturnSame() {
+    public void givenIntegers_whenThirdNumberIsMax_ReturnSame() {
         Integer result = genericsMaximum.testMax(2, 3, 5);
         Assertions.assertEquals(5, result);
     }
 
+    @Test
+    public void givenFloat_whenFirstNumberIsMax_ReturnSame() {
+        Float result = genericsMaximum.testMax(5.2f, 3.2f, 2.2f);
+        Assertions.assertEquals(5.2f, result);
+    }
+
+    @Test
+    public void givenFloat_whenSecondNumberIsMax_ReturnSame() {
+        Float result = genericsMaximum.testMax(3.2f, 5.2f, 2.2f);
+        Assertions.assertEquals(5.2f, result);
+    }
+
+    @Test
+    public void givenIntegers_whenThirdIntegerIsMax_ReturnSame() {
+        Float result = genericsMaximum.testMax(2.2f, 3.2f, 5.2f);
+        Assertions.assertEquals(5.2f, result);
+    }
 }
